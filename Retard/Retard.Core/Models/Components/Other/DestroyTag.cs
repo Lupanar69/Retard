dc4bd9a8fa4ef801502d01398f55d01b6b042adf
Lupanar <69lupanar@gmail.com>
2024-02-17 19:27:09 +0100

    suppression du projet Monogame

--- Retard/Retard.Core/Models/Components/Other/DestroyTag.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Retard.Core.Models.Components.Other
-{
-    /// <summary>
-    /// Tag indiquant que l'entité doit être détruite
-    /// </summary>
-    internal class DestroyTag
-    {
-    }
-}
